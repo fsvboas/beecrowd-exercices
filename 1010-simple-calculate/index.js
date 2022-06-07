d96050cf940f19395712cda5016cee530f12e486
fsvboas <felippesantosvb@hotmail.com>
2022-06-07 14:27:45 -0300

    fix 1010 to beecrowd

--- 1010-simple-calculate/index.js
+++ 1010-simple-calculate/index.js
@@ -3,18 +3,52 @@ const lines = input.toString().split("\n");
 
 console.log(lines);
 
-const product1 = input[0];
-const product2 = input[1];
+// ------------- MINHA RESOLUÇÃO -------------
+// const product1 = input[0];
+// const product2 = input[1];
 
-const dataProduct1 = product1.split(" ");
-const dataProduct2 = product2.split(" ");
+// const dataProduct1 = product1.split(" ");
+// const dataProduct2 = product2.split(" ");
 
-console.log(dataProduct1);
-console.log(dataProduct2);
+// console.log(dataProduct1);
+// console.log(dataProduct2);
 
-const totalProduct1 = dataProduct1[1] * dataProduct1[2];
-const totalProduct2 = dataProduct2[1] * dataProduct2[2];
+// const totalProduct1 = dataProduct1[1] * dataProduct1[2];
+// const totalProduct2 = dataProduct2[1] * dataProduct2[2];
 
-const valueToPay = totalProduct1 + totalProduct2;
+// const valueToPay = totalProduct1 + totalProduct2;
 
-console.log("VALOR A PAGAR: R$ " + valueToPay.toFixed(2));
+// console.log("VALOR A PAGAR: R$ " + valueToPay.toFixed(2));
+
+// ------------- RESOLUÇÃO PARA O BEECROWD -------------
+/**
+ *
+ * @param {String[]} lines
+ */
+const resolution = (lines) => {
+  const adaptParams = (lines) => {
+    // desestruturar o array, separar por espaços e transformar os dados em números
+    return lines.filter(Boolean).map((line) => {
+      const [code, amount, price] = line.split(" ").map(Number);
+      return { code, amount, price };
+    });
+  };
+
+  // calcular o valor dos produtos
+  const calcProductTotalPrice = ({ amount, price }) => amount * price;
+  // somar o valor atual com o preço
+  const totalizeProductPrice = (totalPrices) => {
+    return totalPrices.reduce((acc, price) => acc + price, 0);
+  };
+  const formatAnswer = (price) => `VALOR A PAGAR: R$ ${price.toFixed(2)}`;
+  const products = adaptParams(lines);
+
+  // mapeamento do preço de cada produto no array
+  const allProductsPrice = products.map(calcProductTotalPrice);
+
+  // valor total a pagar pelo produto
+  const valueToPay = totalizeProductPrice(allProductsPrice);
+  const formattedAnswer = formatAnswer(valueToPay);
+  return formattedAnswer;
+};
+console.log(resolution(lines));
