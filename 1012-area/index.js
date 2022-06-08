a4f3bea0393a0f68ee88d5db7c8afbf1552d2958
fsvboas <felippesantosvb@hotmail.com>
2022-06-07 22:37:45 -0300

    change 1012 resolution

--- 1012-area/index.js
+++ 1012-area/index.js
@@ -4,22 +4,27 @@ var lines = input.split(" ");
 const resolution = (lines) => {
   const pi = 3.14159;
 
-  const [a, b, c] = lines.map(parseFloat);
-  
-  const calcAreaRectangledTriangle = parseFloat(a * c) / 2;
+  const adaptParams = () => {
+    const [a, b, c] = lines.map(parseFloat);
+    console.log(a, b, c);
+    return { a, b, c };
+  };
+
+  const { a, b, c } = adaptParams();
+
+  const calcAreaRectangledTriangle = (a * c) / 2;
   console.log("TRIANGULO:", calcAreaRectangledTriangle.toFixed(3));
 
-  const calcAreaRadiusCircle = parseFloat(pi * Math.pow(c, 2));
+  const calcAreaRadiusCircle = pi * Math.pow(c, 2);
   console.log("CIRCULO:", calcAreaRadiusCircle.toFixed(3));
 
-  const calcAreaTrapezium = parseFloat((a + b) * c) / 2;
+  const calcAreaTrapezium = ((a + b) * c) / 2;
   console.log("TRAPEZIO:", calcAreaTrapezium.toFixed(3));
 
-  const calcAreaSquare = parseFloat(b * b);
+  const calcAreaSquare = b * b;
   console.log("QUADRADO:", calcAreaSquare.toFixed(3));
 
-  const calcArearectangle = parseFloat(a * b);
+  const calcArearectangle = a * b;
   console.log("RETANGULO:", calcArearectangle.toFixed(3));
-
 };
 resolution(lines);
