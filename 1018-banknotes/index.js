915fb5cd5dd9bb90a07b98def7a0275888038df9
fsvboas <felippesantosvb@hotmail.com>
2022-06-09 17:41:01 -0300

    1018

---
+++ 1018-banknotes/index.js
@@ -0,0 +1,20 @@
+var input = "576";
+var lines = input.split(" ");
+
+const adaptParams = () => {
+  let [moneyValue] = lines.map(parseInt);
+  return { moneyValue };
+};
+
+const resolution = () => {
+  let { moneyValue } = adaptParams();
+
+  const billNotes = [100, 50, 20, 10, 5, 2, 1];
+
+  billNotes.forEach((item) => {
+    const totalBillNotes = parseInt(moneyValue / item);
+    console.log(`${totalBillNotes} nota(s) de R$ ${item},00`);
+    moneyValue %= item;
+  });
+};
+resolution();
